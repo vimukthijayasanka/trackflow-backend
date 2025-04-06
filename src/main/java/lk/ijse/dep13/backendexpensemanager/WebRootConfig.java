d7979770b8cfea9202f538283092c145fe9ad6ca
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-04-06 23:02:06 +0530

    Register database connection bean in root context

--- src/main/java/lk/ijse/dep13/backendexpensemanager/WebRootConfig.java
+++ src/main/java/lk/ijse/dep13/backendexpensemanager/WebRootConfig.java
@@ -1,8 +1,28 @@
 package lk.ijse.dep13.backendexpensemanager;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.context.annotation.RequestScope;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
+@PropertySource("classpath:/application.properties")
 public class WebRootConfig {
 
+    public WebRootConfig() throws ClassNotFoundException {
+        Class.forName("org.postgresql.Driver");
+    }
+
+    @Bean
+    @RequestScope /// need to clarify
+    public Connection getConnection(@Value("${app.datasource.url}")
+                                    String url, @Value("${app.datasource.username}") String username,
+                                    @Value("${app.database.password}") String password ) throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
 }
--- src/main/java/lk/ijse/dep13/backendexpensemanager/api/UserAuthenticationHttpController.java
+++ src/main/java/lk/ijse/dep13/backendexpensemanager/api/UserAuthenticationHttpController.java
@@ -1,11 +1,16 @@
 package lk.ijse.dep13.backendexpensemanager.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Connection;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
 public class UserAuthenticationHttpController {
+
+    @Autowired
+    private Connection connection;
 
     @PostMapping("/login")
     public String login(){
