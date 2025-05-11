b2eb5b78fb13774ddac418675613f5a67a372222
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-12 02:41:46 +0530

    Add a sameSiteFilter and a cookieConfig

--- src/main/java/lk/ijse/dep13/backendexpensemanager/WebAppConfig.java
+++ src/main/java/lk/ijse/dep13/backendexpensemanager/WebAppConfig.java
@@ -1,8 +1,11 @@
 package lk.ijse.dep13.backendexpensemanager;
 
+import jakarta.servlet.Filter;
+import jakarta.servlet.http.HttpServletResponse;
 import lk.ijse.dep13.backendexpensemanager.interceptor.SecurityInterceptor;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,8 @@ import org.springframework.web.multipart.support.MultipartFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collection;
 
 
 @Configuration
@@ -41,5 +46,42 @@ public class WebAppConfig implements WebMvcConfigurer {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    public ServletContextInitializer initializer() {
+        return servletContext -> {
+            servletContext.getSessionCookieConfig().setSecure(true); // important for HTTPS
+            servletContext.getSessionCookieConfig().setHttpOnly(true);
+            servletContext.getSessionCookieConfig().setName("JSESSIONID");
+            servletContext.getSessionCookieConfig().setPath("/");
+        };
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> sameSiteFilter() {
+        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter((request, response, chain) -> {
+            chain.doFilter(request, response);
+            if (response instanceof HttpServletResponse) {
+                HttpServletResponse res = (HttpServletResponse) response;
+                Collection<String> headers = res.getHeaders("Set-Cookie");
+                boolean firstHeader = true;
+                for (String header : headers) {
+                    if (header.contains("JSESSIONID")) {
+                        String updatedHeader = header + "; SameSite=None; Secure";
+                        if (firstHeader) {
+                            res.setHeader("Set-Cookie", updatedHeader);
+                            firstHeader = false;
+                        } else {
+                            res.addHeader("Set-Cookie", updatedHeader);
+                        }
+                    }
+                }
+            }
+        });
+        return registrationBean;
+    }
+
+
 
 }
